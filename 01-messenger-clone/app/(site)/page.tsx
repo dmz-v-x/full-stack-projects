47380d13bd1098847b658c382fe150e313e7df46
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-03-22 12:12:41 +0000

    Auth UI

--- 01-messenger-clone/app/(site)/page.tsx
+++ 01-messenger-clone/app/(site)/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import AuthForm from "./components/AuthForm";
 export default function Home() {
   return (
     <div
@@ -32,7 +32,7 @@ export default function Home() {
                 </h2>
 
         </div>
-        {/*AuthForm*/}
+        <AuthForm />
     </div>
   );
 }
